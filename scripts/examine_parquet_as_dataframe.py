1ffc98d1cfbc75c8f78d35d8d99a6b29ca91c12b
Jelle Visser <jelle@web-iq.com>
2025-11-07 12:48:15 +0100

    Implement polarsdfwrapper, script for printing dataframe info

---
+++ scripts/examine_parquet_as_dataframe.py
@@ -0,0 +1,24 @@
+import argparse
+import logging
+from pathlib import Path
+
+import polars
+
+from tastebud.polars_df_wrapper import PolarsDfWrapper
+
+logging.basicConfig(
+    format="%(asctime)s,%(msecs)03d %(levelname)-1s [%(filename)s:%(lineno)d] %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+    level=logging.DEBUG,
+)
+logger = logging.getLogger()
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("parquet_file", type=Path)
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = get_args()
+    polars_wrapper = PolarsDfWrapper.from_parquet(args.parquet_file)
+    polars_wrapper.print_df_info()
