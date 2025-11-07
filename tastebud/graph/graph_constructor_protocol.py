e5256276f4d8b6bd52e5958e9d25213b5e4541c1
Jelle Visser <jelle@web-iq.com>
2025-11-07 13:45:39 +0100

    Add ruff.toml, fix ruff complaints

--- tastebud/graph/graph_constructor_protocol.py
+++ tastebud/graph/graph_constructor_protocol.py
@@ -1,7 +1,5 @@
-from abc import ABC
 from typing import Protocol
 
 
 class GraphConstructor(Protocol):
-    def construct_graph(self) -> None:
-        ...
+    def construct_graph(self) -> None: ...
--- tastebud/util/file_util.py
+++ tastebud/util/file_util.py
@@ -1,16 +1,15 @@
 import json
 import logging
-from glob import glob
 from pathlib import Path
 
 logger = logging.getLogger()
 
 
 def load_json_from_file(json_path: Path) -> dict:
-    with open(json_path, encoding="utf-8") as f:
+    with json_path.open(encoding="utf-8") as f:
         data = json.load(f)
     return data
 
 
 def get_files_in_dir(dir_path: Path, file_pattern: str) -> list[Path]:
-    return [Path(str_path) for str_path in glob(str(dir_path / file_pattern))]
+    return [Path(str_path) for str_path in dir_path.glob(file_pattern)]
