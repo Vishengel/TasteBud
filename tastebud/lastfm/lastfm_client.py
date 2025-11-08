5881278cf99c6377203ac103cef282e9c3da798c
Jelle Visser <jelle@web-iq.com>
2025-11-08 15:36:01 +0100

    implement cache for LastFMClient

--- tastebud/lastfm/lastfm_client.py
+++ tastebud/lastfm/lastfm_client.py
@@ -1,13 +1,38 @@
+import logging
+
 import pylast
+from diskcache import Cache
+from pylast import MalformedResponseError
+
+from tastebud.config import CONFIG
+
+logger = logging.getLogger(__name__)
 
 
 class LastFMClient:
+    artist_cache = Cache(CONFIG.cache_dir / "lastfm_similar_artists")
+
     def __init__(self, api_key: str, api_secret: str, username: str, password: str):
         self.network = pylast.LastFMNetwork(
             api_key=api_key, api_secret=api_secret, username=username, password_hash=pylast.md5(password)
         )
 
     def get_similar_artists(self, artist_name: str, limit: int | None = 10) -> list[str]:
+        cache_key = f"{artist_name}{limit}" if limit is not None else artist_name
+        cached_response = self.artist_cache.get(cache_key)
+        if cached_response is not None:
+            logger.debug(f"Using cached similar artists for {artist_name}")
+            return cached_response
+
         artist = self.network.get_artist(artist_name)
-        similar_artists = artist.get_similar(limit=limit)
-        return [similar_artist.item.name for similar_artist in similar_artists]
+
+        try:
+            similar_artists = artist.get_similar(limit=limit)
+        except MalformedResponseError:
+            logger.error(f"Error fetching similar artists for {artist_name}")
+            return []
+
+        similar_artist_names = [similar_artist.item.name for similar_artist in similar_artists]
+        self.artist_cache.set(cache_key, similar_artist_names)
+
+        return similar_artist_names
