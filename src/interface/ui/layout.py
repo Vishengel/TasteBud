65b3754e3f6c9102b9c3a2f0005453e99db926c1
Jelle <jellevisser1994@hotmail.com>
2026-05-14 20:00:39 +0200

    Fix input field bug

--- src/interface/ui/layout.py
+++ src/interface/ui/layout.py
@@ -9,6 +9,23 @@ class NiceGUIPage(Protocol):
 
 async def common_layout(nicegui_page: NiceGUIPage):
     dark = ui.dark_mode(True)
+    ui.add_css("""
+        input:-webkit-autofill,
+        input:-webkit-autofill:hover,
+        input:-webkit-autofill:focus {
+            -webkit-box-shadow: 0 0 0px 1000px #f3f4f6 inset !important;
+            -webkit-text-fill-color: black !important;
+        }
+        body.body--dark input:-webkit-autofill,
+        body.body--dark input:-webkit-autofill:hover,
+        body.body--dark input:-webkit-autofill:focus {
+            -webkit-box-shadow: 0 0 0px 1000px #1f2937 inset !important;
+            -webkit-text-fill-color: white !important;
+        }
+        body.body--dark input {
+            color: white !important;
+        }
+    """)
 
     with ui.header().classes("items-center justify-between bg-gray-800 text-white p-3"):
         ui.label("Tastebud").classes("text-h5")
